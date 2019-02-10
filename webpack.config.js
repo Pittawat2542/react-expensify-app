b46b1fcc8863288d9528a6e4ce3674745c692d53
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-02-10 19:45:47 +0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,53 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  },
-  entry: "./src/app.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        test: /\.js$/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
+module.exports = env => {
+  const isProdcution = env === "production";
+
+  return {
+    devtool: isProdcution ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    },
+    entry: "./src/app.js",
+    mode: "development",
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          test: /\.js$/
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            isProdcution ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: isProdcution ? "[name].[hash].css" : "[name].css",
+        chunkFilename: isProdcution ? "[id].[hash].css" : "[id].css"
+      })
     ]
-  },
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  }
+  };
 };
